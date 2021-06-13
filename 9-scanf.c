2ef2329a57cf4023027c6340ff453e2318cdbfe7
Sayudha Lukita Wibisana <sayudhawibisana@gmail.com>
2021-06-13 14:57:05 +0700

    chapter 9 - scanf

---
+++ 9-scanf.c
@@ -0,0 +1,29 @@
+// This is a sample program that asks users for some basic data and prints it on screen in order to show what was entered
+
+#include <stdio.h>
+
+int main()
+{
+    // Setup the variables that scanf will fill
+    char firstInitial;
+    char lastInitial;
+    int age;
+    int favorite_number;
+
+    printf("What letter does your first name begin with?\n");
+    scanf(" %c", &firstInitial);
+
+    printf("What letter does your last name begin with?\n");
+    scanf(" %c", &lastInitial);
+
+    printf("How old are you?\n");
+    scanf(" %d", &age);
+
+    printf("What is your favorite number?\n");
+    scanf(" %d", &favorite_number);
+
+    printf("\nYour initials are %c. %c. and you are %d years old", firstInitial, lastInitial, age);
+    printf("\nYour favorite number is %d.\n\n", favorite_number);
+
+    return 0;
+}
